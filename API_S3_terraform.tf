7499083b80df6065bf191fb783c189bf07956917
Jennifer <44102740+halloda@users.noreply.github.com>
2021-08-20 11:40:59 -0500

    Update API_S3_terraform.tf

--- API_S3_terraform.tf
+++ API_S3_terraform.tf
@@ -1,3 +1,13 @@
+
+#-----create lambda funtion that runs zipped API_s3.py
+resource "aws_lambda_function" "upload_file" {
+  filename="s3_bucket_upload.zip"
+  function_name="uploadFile"
+  role="aws_iam_role.iam_for_lambda" #placeholder 
+  handler="index.handler"
+}
+
+#-----create the cloudwatch event rule that schedules the lambda function every 5 minutes
 resource "aws_cloudwatch_event_rule" "every_five_minutes" {
   name                = "every-five-minutes"
   description         = "Uploads File Every Five minutes"
@@ -5,11 +15,11 @@ resource "aws_cloudwatch_event_rule" "every_five_minutes" {
 }
 
 
-
+#-----create the cloudwatch event target 
 resource "aws_cloudwatch_event_target" "upload_every_five_mins" {
-  rule      = "${aws_cloudwatch_event_rule.every_five_minutes.name}"
+  rule      = aws_cloudwatch_event_rule.every_five_minutes.name
   target_id = "lambda"
-  arn       = "${aws_lambda_function.lambda.arn}"
+  arn       = aws_lambda_function.lambda.arn
 }
 
 
@@ -18,7 +28,7 @@ resource "aws_cloudwatch_event_target" "upload_every_five_mins" {
 resource "aws_lambda_permission" "permissions_for_lambda" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda.function_name}"
+  function_name = aws_lambda_function.upload_file
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.every_one_minute.arn}"
+  source_arn    = "arn:aws:events:sample_arn" #placeholder
 }
